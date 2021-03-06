f5d555a202039b9e2b3598a1ddec585921d6a0df
Paul <pmikalh@gmail.com>
2021-03-05 17:04:52 -0800

    fucking dank it's all working now

--- index.js
+++ index.js
@@ -17,37 +17,32 @@ let god = '';
 
 console.log('Welcome to the God Module...\n\n');
 
-readline.setPrompt('Enter the name of a god. The options are Guan Yu, Pagu and Jade Emperor. They will tell you some wisdom for the day...\n\n');
+readline.setPrompt('Enter the name of a god. The options are Guan Yu, Pagu and Jade Emperor. They will tell you some wisdom for the day...Press CNTRL + C to quit when you\'ve had enough...\n\n');
 
 readline.prompt();
 
-readline.on('line'),function(input){};
 
-while (!(god_options.includes(input))){
-  console.log('\n\nIncorrect input, type in one of the names from the list or press CNTRL+C to exit\n\n');
-  readline.on(`line`,function(input){});
-};
-
-/*readline.on(`line`, function(input) {
+readline.on(`line`, function(input) {
   input = input.toLowerCase().trim();
-  while (!(god_options.includes(input))) {
-    console.log('\n\nIncorrect input, type in one of the names from the list or press CNTRL+C to exit\n\n');
+  if (!(god_options.includes(input))) {
+    console.log('\n\nIncorrect input, type in one of the names from the list...\n\n');
     readline.prompt();
   };
-  while (input === 'guan yu'){
+  if (input === 'guan yu'){
     console.log('\n\n' + god_data.guan_Yu[getRandomInt(0,2)]);
-    console.log('\n\nTake that wisdom to heart...You can keep receiving wisdom forever... Press CNTRL + C to quit when you\'ve had enough...\n\n');
+    console.log('\n\nTake that wisdom to heart...You can keep receiving wisdom forever... \n\n');
     readline.prompt();
   };
-  while (input === 'pagu'){
+  if (input === 'pagu'){
     console.log('\n\n' + god_data.pagu[getRandomInt(0,2)]);
-    console.log('\n\nTake that wisdom to heart...You can keep receiving wisdom forever... Press CNTRL + C to quit when you\'ve had enough...\n\n');
+    console.log('\n\nTake that wisdom to heart...You can keep receiving wisdom forever... \n\n');
     readline.prompt();
   };
-  while (input === 'jade emperor'){
+  if (input === 'jade emperor'){
     console.log('\n\n' + god_data.jade_Emperor[getRandomInt(0,2)]);
-    console.log('\n\nTake that wisdom to heart...You can keep receiving wisdom forever... Press CNTRL + C to quit when you\'ve had enough...\n\n');
+    console.log('\n\nTake that wisdom to heart...You can keep receiving wisdom forever... \n\n');
     readline.prompt();
   };
-readline.close();
-});*\
+});
+
+
